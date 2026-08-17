5774ee86c2abca1779e2b5b3672f475540f1df1f
Ronny kisbye <111735396+Ronnykisbye@users.noreply.github.com>
2026-08-17 18:13:03 +0200

    Refresh PWA cache for Machine Handover v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "maskinoverdragelse-v1";
+const CACHE = "machine-handover-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
