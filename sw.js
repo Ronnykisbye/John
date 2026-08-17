12875fe906ffe125ccbe4a77e9dca646dc6639a7
Ronny kisbye <111735396+Ronnykisbye@users.noreply.github.com>
2026-08-17 18:33:40 +0200

    Refresh PWA cache for 3D fields and install guide

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "machine-handover-v3";
+const CACHE = "machine-handover-v4";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
